6c9af47af16a44607c8c713812ee4803e65a7887
Gauthier <lhomme.gaut@gmail.com>
2022-04-25 15:51:01 +0200

    create invoice params and index route

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -7,15 +7,26 @@ import {
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
-        <Route
+<Routes>
+  <Route path="/" element={<App />}>
+    <Route path="expenses" element={<Expenses />} />
+    <Route path="invoices" element={<Invoices />}>
+      <Route
+        index
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>Select an invoice</p>
+          </main>
+        }
+      />
+      <Route path=":invoiceId" element={<Invoice />} />
+    </Route>
+    <Route
       path="*"
       element={
         <main style={{ padding: "1rem" }}>
@@ -23,8 +34,8 @@ render(
         </main>
       }
     />
-      </Route>
-    </Routes>
+  </Route>
+</Routes>
   </BrowserRouter>,
   rootElement
 );
